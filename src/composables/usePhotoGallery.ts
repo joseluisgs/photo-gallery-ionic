2ac7090f535bfed6c40d761c240bd8986b0376ba
joseluisgs <joseluisgonsan@gmail.com>
2021-05-02 11:02:25 +0200

    Salvar fotos

--- src/composables/usePhotoGallery.ts
+++ src/composables/usePhotoGallery.ts
@@ -5,9 +5,42 @@ import {
 } from '@capacitor/core';
 
 export function usePhotoGallery() {
-  const { Camera } = Plugins;
+  const { Camera, Filesystem } = Plugins;
   // Mi array reactivo
   const photos = ref<Photo[]>([]);
+
+  // Convierte a Base64
+  const convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onerror = reject;
+    reader.onload = () => {
+      resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  });
+
+  // Salva una imagen
+  const savePicture = async (photo: CameraPhoto, fileName: string): Promise<Photo> => {
+    let base64Data = '';
+
+    // Fetch the photo, read as a blob, then convert to base64 format
+    const response = await fetch(photo.webPath!);
+    const blob = await response.blob();
+    base64Data = await convertBlobToBase64(blob) as string;
+
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: FilesystemDirectory.Data,
+    });
+
+    // Use webPath to display the new image instead of base64 since it's
+    // already loaded into memory
+    return {
+      filepath: fileName,
+      webviewPath: photo.webPath,
+    };
+  };
 
   // Toma una foto
   const takePhoto = async () => {
@@ -18,14 +51,12 @@ export function usePhotoGallery() {
     });
 
     const fileName = `${new Date().getTime()}.jpeg`;
-    const savedFileImage = {
-      filepath: fileName,
-      webviewPath: cameraPhoto.webPath,
-    };
+    const savedFileImage = await savePicture(cameraPhoto, fileName);
 
     photos.value = [savedFileImage, ...photos.value];
   };
 
+  // Lo que devolvemos
   return {
     photos,
     takePhoto,
